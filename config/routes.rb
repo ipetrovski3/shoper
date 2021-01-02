6bd4aaf983dabf6f1829f1c45b5e17d161178a78
ipetrovski3 <ipetrovski3@gmail.com>
2021-01-02 11:37:30 +0100

    auto generated files

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+
+  resources :categories do
+    resources :products, shallow: true
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
